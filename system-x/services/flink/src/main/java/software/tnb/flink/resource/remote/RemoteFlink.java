70b34b55d89df76cacedc513235d86f81aa57116
Vaibhav-Nazare <87978158+Vaibhav-Nazare@users.noreply.github.com>
2024-04-16 09:59:12 +0530

    Add remote service for Flink

---
+++ system-x/services/flink/src/main/java/software/tnb/flink/resource/remote/RemoteFlink.java
@@ -0,0 +1,14 @@
+package software.tnb.flink.resource.remote
+
+import software.tnb.common.deployment.RemoteService;
+import software.tnb.flink.Flink;
+
+import com.google.auto.service.AutoService;
+
+@AutoService(Flink.class)
+public class RemoteFlink extends Flink implements RemoteService {
+    @Override
+    public String host() {
+        return RemoteService.super.host();
+    }
+}
